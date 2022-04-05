762cc1fe44cceee2ca94216c9fa9b8da566562a4
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-04-05 12:48:06 +0200

    add code in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,28 @@ L'utente indica un livello di difficoltà in base al quale viene generata una gr
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. 
+
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+
+In seguito l'utente clicca su una cella:
+    - se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba
+    - la cella si colora di rosso e la partita termina,
+    - altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+La partita termina quando:
+ - il giocatore clicca su una bomba
+    - o raggiunge il numero massimo possibile di numeri consentiti.
+
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+BONUS: 1
+quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
+
+
+*/
 
 
 
@@ -100,13 +121,26 @@ function selectElements (selector, active_class){
         })
     }
 
+}
+
+//creo una funzione che mi generi una lista di 16 numeri random. I numeri nella lista delle bombe non possono essere duplicati.
 
 
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
 
 
+
+
+
+
+/* generateBomb() */
+
+
+/* return Math.floor(Math.random() * (max - min + 1) ) + min; */
 
 
 
